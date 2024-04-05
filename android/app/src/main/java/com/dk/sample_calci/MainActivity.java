aab71cecaa5dbb0d772e69da60c9f54fddf8e6d2
ndhineshkumaar <ndhineshkumaar@gmail.com>
2024-04-05 15:18:44 +0530

    Basic

---
+++ android/app/src/main/java/com/dk/sample_calci/MainActivity.java
@@ -0,0 +1,6 @@
+package com.dk.sample_calci;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Calculator_Screen.dart
@@ -0,0 +1,226 @@
+
+import 'package:flutter/material.dart';
+
+import 'button_values.dart';
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+
+  String number1="";
+  String operand="";
+  String number2="";
+
+  @override
+  Widget build(BuildContext context) {
+    final screensize=MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+      bottom: false,
+      child: Column(
+      children: [
+      Expanded(
+        child: SingleChildScrollView(
+          reverse: true,
+          child: Container(
+            alignment: Alignment.bottomRight,
+            padding:const EdgeInsets.all(16),
+            child: Text(
+              "$number1$operand$number2".isEmpty?"0":"$number1$operand$number2",
+          style:const TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign:  TextAlign.end,
+          ),
+          ),
+        ),
+      ),
+
+        //buttons
+        Wrap(
+          children: Btn.buttonValues
+              .map(
+                  (value) => SizedBox(
+                      width:value==Btn.n0?screensize.width/2: (screensize.width/4),
+                      height: screensize.width/5,
+                      child: buildButton(value))
+          )
+              .toList(),
+        )
+    ],
+    ),),
+    ) ;
+  }
+
+  Widget buildButton(value){
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Material(
+        color: getBtnColor(value),
+        clipBehavior: Clip.hardEdge,
+        shape: OutlineInputBorder(
+          borderSide: BorderSide(color:  Colors.white24),
+          borderRadius: BorderRadius.circular(100),
+        ),
+        child: InkWell(
+          onTap: () => onBtnTap(value),
+          child: Center(
+              child: Text(value,style:  TextStyle(fontWeight: FontWeight.bold,fontSize: 24),)),
+        ),
+      ),
+    );
+  }
+
+  void onBtnTap(String value){
+    if(value==Btn.del){
+      delete();
+      return;
+    }
+
+    if(value==Btn.clr){
+      clear();
+      return;
+    }
+
+    if(value==Btn.per){
+      convertToPercentage();
+      return;
+    }
+
+    if(value==Btn.calculate){
+      calculate();
+      return;
+    }
+
+
+    appendvalue(value);
+  }
+  // calculates the result
+  void calculate(){
+    if(number1.isEmpty) return;
+    if(operand.isEmpty) return;
+    if(number2.isEmpty) return;
+
+    final double num1=double.parse(number1);
+    final double num2=double.parse(number2);
+
+    var result=0.0;
+    switch(operand){
+      case Btn.add:
+        result=num1+num2;
+        break;
+      case Btn.subtract:
+        result=num1-num2;
+        break;
+      case Btn.multiply:
+        result=num1*num2;
+        break;
+      case Btn.divide:
+        result=num1/num2;
+        break;
+      default:
+    }
+    setState(() {
+      number1="$result";
+
+      if(number1.endsWith(other))
+    });
+
+  }
+
+  //percentage convertion
+
+  void convertToPercentage(){
+    if(number1.isNotEmpty && operand.isNotEmpty && number2.isNotEmpty){
+      //calculate
+      //TODO
+    }
+    else if(operand.isNotEmpty){
+      //cannot be converted
+      return;
+    }
+    final number=double.parse(number1);
+    setState(() {
+      number1="${(number/100)}";
+      operand="";
+      number2="";
+    });
+  }
+
+
+  //clear totally
+
+  void clear(){
+    setState(() {
+      number1="";
+      operand="";
+      number2="";
+
+    });
+  }
+
+  //delete one from the end
+
+  void delete(){
+    if(number2.isNotEmpty){
+      number2=number2.substring(0,number2.length-1);
+    }else if(operand.isNotEmpty){
+      operand="";
+    }else if(number1.isNotEmpty){
+      number1=number1.substring(0,number1.length-1);
+    }
+
+    setState(() {
+
+    });
+  }
+
+  void appendvalue(String value){
+    if(value!=Btn.dot && int.tryParse(value)==null){
+
+      if(operand.isNotEmpty && number2.isNotEmpty){
+        //TODO cal equation
+      }
+
+      operand=value;
+    }
+    else if(number2.isEmpty || operand.isNotEmpty){
+      if(value==Btn.dot && number1.contains(Btn.dot)){
+        return;
+      }
+      if(value==Btn.dot &&(number2.isEmpty || number1==Btn.n0)){
+        value="0.";
+      }
+      number1+=value;
+    } else if(number2.isEmpty || operand.isEmpty){
+      if(value==Btn.dot && number2.contains(Btn.dot)){
+        return;
+      }
+      if(value==Btn.dot &&(number2.isEmpty || number2==Btn.n0)){
+        value="0.";
+      }
+      number2+=value;
+    }
+
+    setState(() {
+    });
+  }
+
+
+
+  Color getBtnColor(value){
+    return  [Btn.del,Btn.clr].contains(value)?Colors.blueGrey:
+    [Btn.per,
+      Btn.multiply,
+      Btn.add,Btn.subtract,
+      Btn.divide,
+      Btn.calculate]
+        .contains(value)?Colors.orange:
+    Colors.black87;
+  }
+}
